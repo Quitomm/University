d20f15ce0806f7ad0add526a3600e6986a3161d0
Quitomm <89556863+Quitomm@users.noreply.github.com>
2023-04-16 22:54:50 +0500

    Update task3.py

--- task3.py
+++ task3.py
@@ -8,18 +8,15 @@ omega = np.sqrt(k / m)  # частота колебаний
 gamma = 0.2 * omega  # коэффициент затухания
 u0 = 1.0  # начальное смещение
 v0 = 0.0  # начальная скорость
+destroy = 5 # момент нарушения равновесия
 
-t = np.linspace(0, 50, 500)
+t = np.linspace(destroy, 50, 500)
 u = np.exp(-gamma / (2 * m) * t) * (u0 * np.cos(omega * np.sqrt(1 - (gamma / (2 * m * omega)) ** 2) * t) + (v0 + gamma / (2 * m) * u0) * np.sin(omega * np.sqrt(1 - (gamma / (2 * m * omega)) ** 2) * t))
+t_destroy = np.insert(t, 0, np.linspace(1, destroy, destroy//1))
+u_destroy = np.insert(u, 0, [0] * destroy)
 
-for i in range(len(u)):
-    if abs(u[i]) >= 0.5:
-        t_eq = t[i]
-        u_eq = u[i]
-        break
-
-plt.plot(t, u)
-plt.plot([t_eq, t_eq], [-1, 1], 'r--')
+plt.plot(t_destroy, u_destroy)
+plt.plot([destroy, destroy], [-1, 1], 'r--')
 plt.xlabel('Время, с')
 plt.ylabel('Смещение, м')
 plt.show()
